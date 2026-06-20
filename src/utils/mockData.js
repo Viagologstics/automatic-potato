dca0b83d2c8489163e83fe979cdb5d64accfae3a
Viagologstics <admin@viagologistics.com>
2026-06-20 12:44:41 +0530

    Add mockRawData to mockData.js

---
+++ src/utils/mockData.js
@@ -0,0 +1,4 @@
+export const mockRawData = [
+  { "Date": "2026-06-01", "Vehicle No.": "KA-01-E-1234", "Type Of Truck": "Open 32ft", "Contract Status": "RUNNING", "Opening kms": "10000", "Closing kms": "10500", "Kms": "0", "Total": "45000", "Cost": "30000", "EMI": "5000", "Payment Received": "40000" },
+  { "Date": "2026-06-02", "Vehicle No.": "MH-12-Q-5678", "Type Of Truck": "Container 24ft", "Contract Status": "COMPLETE", "Opening kms": "22000", "Closing kms": "22300", "Kms": "300", "Total": "0", "Transportation Charges": "25000", "Halting Charges": "2000", "Cost": "18000", "EMI": "4000", "Payment Received": "27000" }
+];
